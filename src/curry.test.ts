75d422b4365032c5cec656573400e59a76d68d79
Vadim Kasatkin <penguin2804@gmail.com>
2021-07-21 21:24:54 +0400

    fix curry types

--- src/curry.test.ts
+++ src/curry.test.ts
@@ -2,15 +2,13 @@ import { curry } from "./curry";
 
 describe("curry", () => {
   it("test func", () => {
-    const myFunc = jest.fn((a: any, b: any, c: any) => a + b + c);
+    const myFunc = (a: number, b: number, c: number, d: number, e: number) =>
+      a + b + c + d + e;
     const curried = curry(myFunc);
     expect(typeof curried).toBe("function");
-    expect(myFunc).toHaveBeenCalledTimes(0);
-    expect(curried(1)(2)(3)).toBe(6);
-    expect(myFunc).toHaveBeenCalledTimes(1);
-    expect(curried(30, -2)(4)).toBe(32);
-    expect(myFunc).toHaveBeenCalledTimes(2);
-    expect(curried(-22, -2, 4)).toBe(-20);
-    expect(myFunc).toHaveBeenCalledTimes(3);
+    expect(curried(1)(2)(3)(3)(3)).toBe(12);
+    expect(curried(1)(2)(3)(3)(3)).toBe(12);
+    expect(curried(30, -2)(4)(-12)(4)).toBe(24);
+    expect(curried(-22, -2, 4, 22, -2)).toBe(0);
   });
 });
--- src/curry.ts
+++ src/curry.ts
@@ -1,9 +1,57 @@
-export function curry<T>(
-  func: (...args: T[]) => any
-): (...args: T[]) => (...args2: T[]) => any {
+interface CurriedFunction1 {
+  (t1: number): number;
+}
+
+interface CurriedFunction2 {
+  (t1: number, t2: number): number;
+  (t1: number): CurriedFunction1;
+}
+
+interface CurriedFunction3 {
+  (t1: number, t2: number, t3: number): number;
+  (t1: number, t2: number): CurriedFunction1;
+  (t1: number): CurriedFunction2;
+}
+
+interface CurriedFunction4 {
+  (t1: number, t2: number, t3: number, t4: number): number;
+  (t1: number, t2: number, t3: number): CurriedFunction1;
+  (t1: number, t2: number): CurriedFunction2;
+  (t1: number): CurriedFunction3;
+}
+
+interface CurriedFunction5 {
+  (t1: number, t2: number, t3: number, t4: number, t5: number): number;
+  (t1: number, t2: number, t3: number, t4: number): CurriedFunction1;
+  (t1: number, t2: number, t3: number): CurriedFunction2;
+  (t1: number, t2: number): CurriedFunction3;
+  (t1: number): CurriedFunction4;
+}
+
+export type CurriedFunction<T> = T extends [
+  number,
+  number,
+  number,
+  number,
+  number
+]
+  ? CurriedFunction5
+  : T extends [number, number, number, number]
+  ? CurriedFunction4
+  : T extends [number, number, number]
+  ? CurriedFunction3
+  : T extends [number, number]
+  ? CurriedFunction2
+  : T extends [number]
+  ? CurriedFunction1
+  : never;
+export function curry<T extends number[]>(
+  fn: (...args: T) => number
+): CurriedFunction<T>;
+export function curry(func: (...args: number[]) => any): any {
   let { length } = func;
-  let argValues: T[] = [];
-  return function rec(...args: T[]): () => any {
+  let argValues: number[] = [];
+  return function rec(...args: number[]): () => any {
     length -= args.length;
     argValues.push(...args);
     if (length === 0) {
@@ -12,7 +60,7 @@ export function curry<T>(
       length = func.length;
       return func(...argPassed);
     }
-    return function (...args2: T[]) {
+    return function inner(...args2: number[]) {
       return rec(...args2);
     };
   };
